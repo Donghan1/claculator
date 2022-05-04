86800761f199b2c668424a4d99ec0b58932af6c5
Donghan1 <100738271+Donghan1@users.noreply.github.com>
2022-05-04 09:46:50 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name='calculator0001',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='kimdonghan',
+  autor_email='jjoksb1@naver.com',
+  yrl="https://github.com/Donghan1/calcutator0001/archive/refs/tags/v.0.0.1zip",
+  install_requires=['pytest'],
+  long_description='oss-dev calculator python midule',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
